b86b2a1de058e7db3989fba877aa7b4acfa36ba8
Muna Hussen <munahussen@Munas-MBP.lan>
2021-06-13 19:55:56 -0400

    Changed Header layout

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ ScrollTrigger.matchMedia({
 
     imgs.forEach(element => {
       gsap.from(element, {
+        toggleActions: "play pause resume reset",
         scrollTrigger: element,
         duration: 1,
         y: 200
@@ -18,6 +19,7 @@ ScrollTrigger.matchMedia({
 
     projectText.forEach(element => {
       gsap.from(element, {
+        toggleActions: "play pause resume reset",
         scrollTrigger: element,
         duration: 1,
         x: -200
@@ -25,22 +27,9 @@ ScrollTrigger.matchMedia({
     });
     },
 
-  // medium
-  "(min-width: 600px) and (max-width: 959px)": function() {
-    // The ScrollTriggers created inside these functions are segregated and get
-    // reverted/killed when the media query doesn't match anymore. 
-  },
-
-  // small
-  "(max-width: 599px)": function() {
-    // The ScrollTriggers created inside these functions are segregated and get
-    // reverted/killed when the media query doesn't match anymore. 
-  },
 	
   // all 
   "all": function() {
-    // ScrollTriggers created here aren't associated with a particular media query,
-    // so they persist.
     gsap.from(".about__info__line", {
       scrollTrigger: ".about__info__line",
       duration: 1.5,
