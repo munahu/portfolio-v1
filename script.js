ca3de7e24c24aeff6c40d5cf0797d67839138ef3
Muna Hussen <munahussen@Munas-MBP.lan>
2021-06-13 15:46:47 -0400

    Added media queries for Work section and wrapper

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
-gsap.registerPlugin(ScrollTrigger);
+ScrollTrigger.matchMedia({
+	
+  // large
+  "(min-width: 960px)": function() {
+    gsap.registerPlugin(ScrollTrigger);
 
-  gsap.from(".about__info__line", {
-    scrollTrigger: ".about__info__line",
-    duration: 1.5,
-    opacity: 0,
-    stagger: .2,
-  })
+    const imgs = gsap.utils.toArray(".work__project__imgContainer");
 
+    imgs.forEach(element => {
+      gsap.from(element, {
+        scrollTrigger: element,
+        duration: 1,
+        y: 200
+      })
+    });
 
-const imgs = gsap.utils.toArray(".work__project__imgContainer");
+    const projectText = gsap.utils.toArray(".work__project__info");
 
-imgs.forEach(element => {
-  gsap.from(element, {
-    scrollTrigger: element,
-    duration: 1,
-    y: 200
-  })
-});
+    projectText.forEach(element => {
+      gsap.from(element, {
+        scrollTrigger: element,
+        duration: 1,
+        x: -200
+      })
+    });
+    },
 
-const projectText = gsap.utils.toArray(".work__project__info");
+  // medium
+  "(min-width: 600px) and (max-width: 959px)": function() {
+    // The ScrollTriggers created inside these functions are segregated and get
+    // reverted/killed when the media query doesn't match anymore. 
+  },
 
-projectText.forEach(element => {
-  gsap.from(element, {
-    scrollTrigger: element,
-    duration: 1,
-    x: -200
-  })
-});
+  // small
+  "(max-width: 599px)": function() {
+    // The ScrollTriggers created inside these functions are segregated and get
+    // reverted/killed when the media query doesn't match anymore. 
+  },
+	
+  // all 
+  "all": function() {
+    // ScrollTriggers created here aren't associated with a particular media query,
+    // so they persist.
+    gsap.from(".about__info__line", {
+      scrollTrigger: ".about__info__line",
+      duration: 1.5,
+      opacity: 0,
+      stagger: .2,
+    })
+  }
+	
+}); 
