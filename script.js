414a2fa10d7a3aba96ad39ad1c1a6fd63198c078
Muna Hussen <munahussen@Munas-MBP.lan>
2021-06-14 23:22:13 -0400

    Added functionality to mobile nav

---
+++ script.js
@@ -0,0 +1,13 @@
+$(".hamburgerIcon").click(function() {
+    $(".navlinks__mobile").toggleClass("hidden");
+    $("body").toggleClass("lock");
+})
+
+$(".navlinks__mobile__link").click(function() {
+    $("body").toggleClass("lock");
+    $(".navlinks__mobile").toggleClass("hidden");
+})
+
+$(".exit").click(function() {
+    $(".navlinks__mobile").toggleClass("hidden");
+})
